87232727679df4a37d29acadaa878532aca378ec
sthevensmk <sthevens.konesukmiranda@gmail.com>
2025-02-13 18:51:48 -0300

    Variáveis e funções no JS

---
+++ aula-0.1-variaveis-e-funcoes/js-aula0.1.js
@@ -0,0 +1,20 @@
+// Comentário em Linha JS
+/*  Comentario
+    Em
+    Bloco      */ 
+// CTRL + ; - Atalho pra comentario
+
+// Declaração de variáveis no JS
+// CONST x LET x VAR
+
+const nome = "Sthevens K.M" // CONST - O valor não muda, pode ser iniciada vazia.
+let Nome // LET - O Valor pode ser flexivel, pode ser iniciada vazia.
+// const nome = "Louco" -> Resulta em Erro.
+// let Nome -> Resulta em Erro.
+var cor = "Azul"
+var cor = "Amarelo"
+let cidade = "Registro"
+cidade = "Jacupiranga" //Let permite alterar o valor.
+
+const message = "Hello, World! Iniciando estudos com Javascript!"
+console.log(message);
